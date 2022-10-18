e6c02586d217d288f6fafa51c83e9157d2401d60
sachanashutosh15 <sachanashutosh15@gmail.com>
2022-10-18 11:41:44 +0530

    final push

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Patch,
+  UseGuards
+} from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { User } from '@prisma/client';
 
-@Controller('user')
-export class UserController {}
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser() {
+
+  }
+}
